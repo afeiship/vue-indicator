cd6050935c89dd69e04659e4ba9bf9e9d06a17fb
feizheng <1290657123@qq.com>
2016-12-08 21:20:02 +0800

    intial

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,13 @@
 module.exports = {
   entry: './src/index.js',
-  // 编译的文件路径
   output: {
-    //`dist`文件夹
     path: './dist',
-    // 文件 `build.js` 即 dist/build.js
     filename: 'build.js'
   },
   module: {
-    // 一些特定的编译规则
     loaders: [{
-      // 让webpack去验证文件是否是.js结尾将其转换
       test: /\.js$/,
-      // 通过babel转换
       loader: 'babel',
-      // 不用转换的node_modules文件夹
       exclude: /node_modules/
     }]
   }
